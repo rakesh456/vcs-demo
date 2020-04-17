491fb0d5084c204e224b1653d1192bc43606c623
GirishRathod <rathodgirishk@gmail.com>
2020-04-17 15:17:34 +0530

    Get permission demo

---
+++ GetMediaPermission/WebRTC.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Put variables in global scope to make them available to the browser console.
+const constraints = window.constraints = {
+  audio: false,
+  video: true
+};
+
+function handleSuccess(stream) {
+  const video = document.querySelector('video');
+  const videoTracks = stream.getVideoTracks();
+  console.log('Got stream with constraints:', constraints);
+  console.log(`Using video device: ${videoTracks[0].label}`);
+  window.stream = stream; // make variable available to browser console
+  video.srcObject = stream;
+}
+
+function errorMsg(msg, error) {
+  const errorElement = document.querySelector('#errorMsg');
+  errorElement.innerHTML += `<p>${msg}</p>`;
+  if (typeof error !== 'undefined') {
+    console.error(error);
+  }
+}
+
+async function init(e) {
+  try {
+    const stream = await navigator.mediaDevices.getUserMedia(constraints);
+    handleSuccess(stream);
+    e.target.disabled = true;
+  } catch (e) {
+    errorMsg(`getUserMedia error: ${e.name}`, e);
+  }
+}
+
+document.querySelector('#permissionButton').addEventListener('click', e => init(e));
